22cc56dc80c1dbdf3241e229946761d362e46295
Noah Praskins <noahpraskins@gmail.com>
2022-04-26 17:22:09 -0700

    asdf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/np-at/go-quickxorhash
 
 go 1.17
+
+require github.com/rclone/rclone v1.57.0
--- qxor/QuickXorHash_test.go
+++ qxor/QuickXorHash_test.go
@@ -1,7 +1,10 @@
 package qxor_test
 
 import (
+	"encoding/base64"
 	"github.com/np-at/go-quickxorhash/qxor"
+	"github.com/rclone/rclone/backend/onedrive/quickxorhash"
+	"io"
 	"log"
 	"os"
 	"testing"
@@ -26,6 +29,27 @@ func TestComputeQuickXorHash(t *testing.T) {
 		t.Errorf("computed hash did not match known value")
 	}
 	file.Close()
+}
+func BenchmarkOther(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		file, err := os.Open(testFilePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		q := quickxorhash.New()
+		_, err = io.Copy(q, file)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		sum := q.Sum(nil)
+		hashString := base64.StdEncoding.EncodeToString(sum)
+
+		if hashString != testFileQuickXorHashValue {
+			log.Println(hashString)
+			b.Error("sums don't line up")
+		}
+		file.Close()
+	}
 }
 func BenchmarkComputeQuickXorHash(b *testing.B) {
 	for i := 0; i < b.N; i++ {
